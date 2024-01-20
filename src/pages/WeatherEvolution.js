b47c29169833e2ce7f10e10f495bd6365516a392
kate <katka.zaprazna@icloud.com>
2024-01-20 13:47:25 +0100

    Integrating chart library

--- src/pages/WeatherEvolution.js
+++ src/pages/WeatherEvolution.js
@@ -1,8 +1,20 @@
 import { Heading, Text } from "@chakra-ui/react";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
 // get the chart working - later?
 
 export const WeatherEvolution = () => {
+  const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
+
+  const renderLineChart = (
+    <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <XAxis dataKey="name" />
+      <YAxis />
+    </LineChart>
+  );
+
   return (
     <div className='content'>
       <Heading as='h3' size='lg'>
@@ -12,6 +24,7 @@ export const WeatherEvolution = () => {
         Aute consequat qui esse exercitation. Consectetur mollit laboris labore
         officia dolore laborum eiusmod sit tempor dolor non.
       </Text>
+      {renderLineChart}
     </div>
   );
 };
